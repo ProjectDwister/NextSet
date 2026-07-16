20670962c4dc2f5b821c97324636219680dff113
Animesh Dwivedi <animeshcred@gmail.com>
2026-07-16 17:33:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this whenever the app-shell file list below changes; old caches
 // get cleaned up automatically on activate.
-const CACHE_NAME = 'nextset-v12';
+const CACHE_NAME = 'nextset-v13';
 
 const APP_SHELL = [
   './index.html',
